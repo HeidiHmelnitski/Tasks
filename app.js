bf1190b56420c09e6d228fa3f00f1985e9428ee7
Heidi <heidi.hmelnitski@khk.ee>
2021-12-01 11:45:22 +0200

    addtask jne

--- app.js
+++ app.js
@@ -10,29 +10,83 @@ tasklist.addEventListener("click", delTask)
 const deletebtn = document.querySelector("#delete-tasks")
 deletebtn.addEventListener( "click" , delTasks)
 
+//pageReload
+document.addEventListener("DOMContentLoad", getTasks)
+
+//getTask
+function getTasks(){
+    let tasks
+    if (localStorage.getItem("tasks") === null) {
+        task = []
+    } else {
+        tasks = JSON.parse(localStorage.getItem("tasks"))
+    }
+
+    //for each task in tasks - create li and add to tasklist
+    task.forEach(function (taskFromLS) {
+        const li = document.createElement("li")
+        // get element from DOM
+        li.className = "collection-item"
+        // get element from DOM
+        const text = document.createTextNode(taskFromLS)
+        // get element from DOM
+        li.appendChild(text)
+        const link= document.createElement("a")
+        link.className ="secondary-content"
+        link.appendChild(document.createTextNode("X"))
+        link.setAttribute("href", "a")
+        li.appendChild(link)
+
+
+        // get element from DO
+        taskList.appendChild(li)
+    })
+}
+
+
 function delTasks () {
 //    tasklist.innerHTML = ""
     while (tasklist.firstChild){
-        tasklist.removeChild(tasklist.firstChild)
+        tasklist.removeChild(tasklist.firstChild);
     }
+    removeAllStorage()
+}
+
+//removeallstorage
+function removeAllStorage(){
+    localStorage.removeItem("tasks")
 }
 
 //delTask
 function delTask(event) {
     if (event.target.textContent === "X") {
         if(confirm("jnejnej")){
-            event.target.parentElement.remove()
-
-         console.log(event.target)
+            event.target.parentElement.remove();
         }
     }
 }
+//removeStorage
+function removeStorage(task) {
+    let tasks
+    if (localStorage.getItem("tasks") === null) {
+        task = []
+    } else {
+        tasks = JSON.parse(localStorage.getItem("tasks"))
+    }
+    task.forEach(function (taskFromLs, taskIndex) {
+        if (taskFromLs === task) {
+            tasks.splice(taskIndex, 1)
+        }
+    })
+    localStorage.setItem("tasks", JSON.stringify(tasks))
+}
 
-// add task funktsion
+
+
+// add task function
 function addTask (event) {
-    console.log(event.type)
     //get task value from form input
-    const task = document.querySelector("#Task").value
+    const taskInput = document.querySelector("#Task").value
     // get element from DOM
     const taskList = document.querySelector("ul")
     // get element from DOM
@@ -45,9 +99,23 @@ function addTask (event) {
     li.appendChild(text)
     const link= document.createElement("a")
     link.className ="secondary-content"
+    link.setAttribute("href", "#")
     link.appendChild(document.createTextNode("X"))
-    link.setAttribute("href", "a")
-    li.appendChild(link)
+    li.appendChild(link);
+    const ul = document.querySelector(".collection");
+    ul.appendChild(li);
+    saveTask(task);
+    taskInput.value = "";
+    event.preventDefault();
+
+    function saveTask(task) {
+        let tasks;
+        tasks = [];
+        tasks.push(task);
+
+    }
+
+
 
 
     // get element from DO
